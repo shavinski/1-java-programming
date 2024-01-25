5ddde84bed777486fd89a1a7221b3fbf9c6b73f2
Jakob <shavinski.jakob@gmail.com>
2024-01-24 17:38:57 -0800

    Finish methods

---
+++ part-2/methods/HoleInGround.java
@@ -0,0 +1,21 @@
+package methods;
+
+import java.util.Scanner;
+
+public class HoleInGround {
+    public static void main(String[] args) {
+        printText();
+    }
+
+    public static void printText() {
+        System.out.println("How many times?");
+        Scanner scanner = new Scanner(System.in);
+
+        int ans = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < ans; i++) {
+            System.out.println("In a hole in the ground there lived a method");
+        }
+
+    }
+}
---
+++ part-2/methods/StarSign.java
@@ -0,0 +1,57 @@
+package methods;
+
+public class StarSign {
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+
+        printSquare(4);
+
+        printRectangle(17, 3);
+
+        printTriangle(4);
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println("");
+    }
+
+    public static void printSquare(int size) {
+        System.out.println("");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        System.out.println("");
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+
+    public static void printTriangle(int size) {
+        System.out.println("");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
