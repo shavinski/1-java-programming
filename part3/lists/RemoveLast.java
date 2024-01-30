7cace7b4c2a568663f1edaf1154591168ed7e743
Jakob <shavinski.jakob@gmail.com>
2024-01-29 19:12:32 -0800

    Finish part 3 Array List

--- part3/lists/RemoveLast.java
+++ part3/lists/RemoveLast.java
@@ -1,7 +1,26 @@
 package part3.lists;
 
+import java.util.ArrayList;
+
 public class RemoveLast {
     public static void main(String[] args) {
-        
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        int lastIndex = strings.size() - 1;
+        strings.remove(lastIndex);
     }
 }
