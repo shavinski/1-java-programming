ef931aa836dcde2f129a235793c853fd03395db6
Jakob Shavinkski <shavinski.jakob@gmail.com>
2024-05-10 21:25:29 -0700

    Finish print thrice

---
+++ part3/usingStrings/PrintThrice.java
@@ -0,0 +1,16 @@
+package part3.usingStrings;
+
+import java.util.Scanner;
+
+public class PrintThrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give a word");
+        String userInput = scanner.nextLine();
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.print(userInput);    
+        }
+    }
+}
