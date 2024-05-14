b487c6158c9e67a1f52df435d127e55f434fdb4c
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-13 18:32:53 -0700

    Finish exercises: Door and Product

---
+++ part4/introToOOP/Door.java
@@ -0,0 +1,11 @@
+package part4.introToOOP;
+
+public class Door {
+    
+    public Door() {
+    }
+
+    public void knock() {
+        System.out.println("Whos there?");
+    }
+}
--- part4/introToOOP/Main.java
+++ part4/introToOOP/Main.java
@@ -2,15 +2,13 @@ package part4.introToOOP;
 
 import part4.introToOOP.Person;
 import part4.introToOOP.Whistle;
-
+import part4.introToOOP.Door;
+import part4.introToOOP.Product;
 
 public class Main {
     public static void main(String[] args) {
-        Whistle duckWhistle = new Whistle("Kvaak");
-        Whistle roosterWhistle = new Whistle("Peef");
+       Product banana = new Product("Banana", 1.1, 13);
 
-        duckWhistle.sound();
-        roosterWhistle.sound();
-        duckWhistle.sound();
+       banana.printProduct();
     }
 }
