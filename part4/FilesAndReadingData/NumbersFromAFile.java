ef5a51b99dbde174d33b877b93c7deebe6e5ea97
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-15 12:14:55 -0700

    Finish exercise: Numbers from a file

--- part4/FilesAndReadingData/NumbersFromAFile.java
+++ part4/FilesAndReadingData/NumbersFromAFile.java
@@ -1,6 +1,36 @@
 package part4.FilesAndReadingData;
 
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class NumbersFromAFile {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("File? ");
+        String fileName = scanner.nextLine();
+
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+
+        int numsInRange = 0;
+
+        try (Scanner scanner2 = new Scanner(Paths.get(fileName))) {
+            while (scanner2.hasNextLine()) {
+                int currentNum = Integer.valueOf(scanner2.nextLine());
+
+                if (currentNum >= lowerBound && currentNum <= upperBound) {
+                    numsInRange++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println(numsInRange);
+
     }
 }
