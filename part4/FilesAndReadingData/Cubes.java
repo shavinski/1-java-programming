23dfac9004fc543d87da2ec46fb4083e3d410356
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-15 10:53:24 -0700

    Finish exercises: Number of strings and cubes

---
+++ part4/FilesAndReadingData/Cubes.java
@@ -0,0 +1,21 @@
+package part4.FilesAndReadingData;
+
+import java.util.Scanner;
+
+public class Cubes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            int inputToInt = Integer.valueOf(input);
+
+            System.out.println(Math.pow(inputToInt, 3));   
+        }
+    }
+}
---
+++ part4/FilesAndReadingData/NumberOfStrings.java
@@ -0,0 +1,23 @@
+package part4.FilesAndReadingData;
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+
+        while(true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
